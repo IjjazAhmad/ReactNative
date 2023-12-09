f73e9736bae0d1a7c66d2547141c80ae2167a28d
IjjazAhmad <ijjazahmad705@gmail.com>
2023-12-10 01:38:46 +0500

    update name

--- myapptwo/src/screens/Frontend/Home.js
+++ myapptwo/src/screens/Frontend/Home.js
@@ -1,27 +1,81 @@
-import { View, Text, Button , TouchableOpacity} from 'react-native'
-import React from 'react'
+import { View, Text, Button, TouchableOpacity, Alert } from 'react-native'
+import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from '../StyleSheet/style'
 import HeaderLogo from '../../components/HeaderLogo'
-export default function Home({navigation}) {
+import { TextInput } from 'react-native-paper';
+import { SafeAreaFrameContext, SafeAreaView } from 'react-native-safe-area-context';
+import { useAuthContaxt } from '../../contaxts/AuthContaxt';
+
+const initialState={title:"", description:""}
+export default function Home({ navigation }) {
+  const [state, setState] =useState(initialState)
+  const {dispatch} = useAuthContaxt()
+  
+    const handleChange=(name, value)=>{
+      setState(s=>({...s, [name]:value}))
+    }
+    const handleSubmite=()=>{
+      let {title, description} = state
+      title = title.trim()
+      description = description.trim()
+      let formData = {title, description}
+      formData.id = Math.random().toString(36).slice(2);
+      formData.dateCreated="firebase.firestore.Fieldvalue.serverTimeStamp()"
+      formData.status= "active"
+      formData.sortOrder = 1
+      formData.createdBy={
+        email:"user.email",
+        uid:"user,uid",
+      }
+      createDocument(formData)
+     
+      // dispatch({type:"Login"})
+    }
+
+const createDocument=(formdata) => {
+  console.log("🚀 ~ file: Home.js:37 ~ createDocument ~ formdata:", formdata)
+  return {
+}
+  
+}
+
+
+
   return (
-    <View style={styles.flexContainer}>
-      <HeaderLogo/>
-      <View style={[styles.box, styles.flexCenter,styles.shadowProp]}>
-      <Text style={styles.h1}>Home <Icon name="home" size={30} color="#fff" /></Text>
+    <SafeAreaView style={styles.flexContainer}>
+      <View style={styles.flexContainer}>
+        <TextInput
+          mode="outlined"
+          label="Title"
+          placeholder="Type something"
+          right={<TextInput.Affix text="/100" />}
+          style={{ width: "100%" }}
+          onChangeText={value => handleChange("title", value)}
+        />
+        <TextInput
+          mode="outlined"
+          label="Description"
+          placeholder="Type something"
+          right={<TextInput.Affix text="/100" />}
+          style={{ width: "100%" }}
+          onChangeText={value => handleChange("description", value)}
+        />
+
+        <View>
+          <TouchableOpacity
+            style={[styles.btn, styles.shadowProp, { minWidth: "70%", alignItems: "center" }]}
+            activeOpacity={0.5}
+            onPress={handleSubmite}
+          >
+            <Text >ADD TODO <Icon name="plus" size={15} /></Text>
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.flexCenter, styles.shadowProp]}>
+          <Button title='GO TO ABOUT' onPress={() => { navigation.navigate("About", { name: "ijjaz", age: 20, status: "active" }) }} />
+        </View>
       </View>
-      <View style={[ styles.flexCenter,styles.shadowProp]}>
-        <Button title='GO TO ABOUT' onPress={()=>{ navigation.navigate("About" ,{name: "ijjaz", age:20, status:"active"}  ) } } />
-      </View>
-      <View>
-        <TouchableOpacity 
-        style={[styles.btn, styles.shadowProp, {minWidth:"70%" , alignItems:"center"}]}
-        activeOpacity={0.5}
-        >
-          <Text >ADD TODO <Icon name="plus" size={15}/></Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- myapptwo/src/screens/StyleSheet/style.js
+++ myapptwo/src/screens/StyleSheet/style.js
@@ -4,7 +4,7 @@ const styles = StyleSheet.create({
   flexContainer: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    paddingHorizontal:10,
   },
   flexCenter: {
     justifyContent: "center",
